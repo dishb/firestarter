464c02d8d841926ea9dc5884e8e384ac7ef2a625
dishb <code.dishb@gmail.com>
2024-08-09 12:31:58 -0700

    edit: remove license reference

--- firestarter/_fuel/__init__.py
+++ firestarter/_fuel/__init__.py
@@ -1,6 +1,5 @@
 from ._ignite import _ignite # noqa: F401
 
 __source__ = "https://github.com/dishb/firestarter"
-__license__ = "MIT License"
 __version__ = "0.0.2"
 __author__ = "Dishant B. (@dishb) <code.dishb@gmail.com>"
