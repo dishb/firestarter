a87a3923e029ab76e7f567dac912835562f836d0
dishb <code.dishb@gmail.com>
2024-09-01 11:56:22 -0700

    delete: remove handling of KeyboardInterrupt

--- firestarter/__main__.py
+++ firestarter/__main__.py
@@ -1,12 +1,7 @@
 from sys import exit as sys_exit
 
 from ._core._entry_points import _console
-from ._core._labels import _Labels
 
 if __name__ == "__main__":
-    try:
-        EXIT_CODE = _console()
-        sys_exit(EXIT_CODE)
-    except KeyboardInterrupt:
-        print("\n" + _Labels.INFO + "Application interrupted and quit with Control+C.\n")
-        sys_exit(130)
+    EXIT_CODE = _console()
+    sys_exit(EXIT_CODE)
