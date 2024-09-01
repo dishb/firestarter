5adfc10b9f2e14760875c3b67e067d6b3b0be27f
dishb <code.dishb@gmail.com>
2024-09-01 14:27:26 -0700

    delete: remove test file from development

--- firestarter/_core/test.py
+++
@@ -1,8 +0,0 @@
-import PyInstaller.__main__
-
-PyInstaller.__main__.run(["main.py",
-                          "-F",
-                          "-n Main",
-                          "-w",
-                          "--disable-windowed-traceback"
-                          ])
