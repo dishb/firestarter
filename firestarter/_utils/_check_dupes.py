68134f9366b64d46624e8f8dd28ec857493e5a5f
dishb <code.dishb@gmail.com>
2024-09-08 10:50:55 -0700

    fix: check for empty lines in _check_dupes.py

--- firestarter/_utils/_check_dupes.py
+++ firestarter/_utils/_check_dupes.py
@@ -3,9 +3,10 @@ def _check_dupes(lines: list) -> bool:
     dupes = []
 
     for line in lines:
-        if line[0] in seen:
-            dupes.append(line[0])
-        else:
-            seen.add(line[0])
+        if line != "":
+            if line[0] in seen:
+                dupes.append(line[0])
+            else:
+                seen.add(line[0])
 
     return len(dupes) == 0
