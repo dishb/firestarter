7601cb90406a49122b6a5e55918abba0f2eda283
dishb <code.dishb@gmail.com>
2025-01-06 17:05:56 +0530

    edit: update tests

--- tests/test_read_file.py
+++ tests/test_read_file.py
@@ -1,4 +1,4 @@
-from firestarter._utils._read_file import _read_file
+from ..firestarter._utils._read_file import _read_file
 
 def test_read_file(tmp_path):
     file = tmp_path / "test.txt"
@@ -8,4 +8,5 @@ $ another comment
 [path] :: /Users/dishb/Documents/"""
     file.write_text(file_content, encoding = "utf-8")
     expected = ["$ comment", "$ another comment", "[name] :: name", "[path] :: /Users/dishb/Documents/"]
+
     assert _read_file(file) == expected
